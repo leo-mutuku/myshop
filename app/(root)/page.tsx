ca81f2eae53ff324e9c6810a925dcfec6b8614ec
leo-mutuku <le.mutuku@gmail.com>
2023-08-09 05:56:47 +0300

    Added a button from shacn UI cool stuff

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-      <p>Hello admin dashboard</p>
+      <div className="p-4">
+        <Button size="default" variant="destructive">Click me</Button>
+      </div>
     )
   }
   
